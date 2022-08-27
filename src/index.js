390ec5dc5019378b0ef92d25b690289d0fdaae2c
RoyOkite <royokite@gmail.com>
2022-08-28 02:59:07 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const init = () => {
-  
-}
+//Fetch on demand with forms
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+    const form = document.querySelector('form')
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(movie => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+  
+        title.innerText = movie.title;
+        summary.innerText = movie.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
